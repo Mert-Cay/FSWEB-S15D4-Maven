1a135ea9504b1b3cc026a7f406e46d6549490c4a
Mrtea-hub <cy_mert@hotmail.com>
2026-09-18 22:43:05 +0300

    S15D4- Project finished

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,9 +1,55 @@
 package org.example;
 
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.Stack;
 
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+    // Testin beklediği Stack tabanlı Palindrom metodu
+    public static boolean checkForPalindrome(String text) {
+        if (text == null) {
+            return false;
+        }
+
+        String cleaned = text.replaceAll("[^a-zA-Z0-9]", "").toLowerCase();
+
+        Stack<Character> stack = new Stack<>();
+        for (char c : cleaned.toCharArray()) {
+            stack.push(c);
+        }
+
+        StringBuilder reversed = new StringBuilder();
+        while (!stack.isEmpty()) {
+            reversed.append(stack.pop());
+        }
+
+        return cleaned.equals(reversed.toString());
     }
 
+    // Decimal to Binary dönüştürme metodu
+    public static String convertDecimalToBinary(int number) {
+        if (number == 0) {
+            return "0";
+        }
+
+        Deque<Integer> stack = new ArrayDeque<>();
+        while (number > 0) {
+            stack.push(number % 2);
+            number /= 2;
+        }
+
+        StringBuilder binary = new StringBuilder();
+        while (!stack.isEmpty()) {
+            binary.append(stack.pop());
+        }
+
+        return binary.toString();
+    }
+
+    public static void main(String[] args) {
+        // İstersen burada elle kontrol edebilirsin:
+        System.out.println(checkForPalindrome("Racecar"));
+        System.out.println(convertDecimalToBinary(13));
+    }
 }
---
+++ src/main/java/org/example/WorkintechList.java
@@ -0,0 +1,36 @@
+package org.example;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class WorkintechList<E> extends ArrayList<E> {
+
+    @Override
+    public boolean add(E e) {
+        if (this.contains(e)) {
+            return false;
+        }
+        return super.add(e);
+    }
+
+    @Override
+    public void add(int index, E element) {
+        if (!this.contains(element)) {
+            super.add(index, element);
+        }
+    }
+
+    @Override
+    public boolean remove(Object o) {
+        boolean removed = super.remove(o);
+        if (removed) {
+            sort();
+        }
+        return removed;
+    }
+
+    @SuppressWarnings({"unchecked", "rawtypes"})
+    public void sort() {
+        Collections.sort((ArrayList) this);
+    }
+}
